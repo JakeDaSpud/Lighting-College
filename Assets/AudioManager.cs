74c31f7feb96e4e334ead0188d062812319b5a00
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2023-10-16 15:03:51 +0100

    Added AudioManager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioClip jumpClip;
+    public AudioClip dashClip;
+    public AudioClip footstepSound;
+    public AudioClip attackSound;
+    public AudioClip backgroundMusic;
+
+    private AudioSource soundEffectSource;
+    private AudioSource backgroundMusicSource;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else {
+            Destroy(gameObject);
+            return;
+        }
+
+        soundEffectSource = gameObject.AddComponent<AudioSource>();
+        backgroundMusicSource = gameObject.AddComponent<AudioSource>();
+
+        //Hard codes single song into Source, makes it loop then play
+        backgroundMusicSource.clip = backgroundMusic;
+        backgroundMusicSource.loop = true;
+        backgroundMusicSource.Play();
+    }
+
+    public void PlayJumpSound() {
+        soundEffectSource.PlayOneShot(jumpClip);
+    }
+
+    public void PlayDashSound() {
+        soundEffectSource.PlayOneShot(dashClip);
+    }
+
+    public void PlayFootstepSound() {
+        soundEffectSource.PlayOneShot(footstepSound);
+    }
+
+    public void PlayAttackSound() {
+        soundEffectSource.PlayOneShot(attackSound);
+    }
+
+    public void PlayBackgroundMusic() {
+        if (!backgroundMusicSource.isPlaying)
+        {
+            backgroundMusicSource.Play();
+        }
+    }
+
+    public void PauseBackgroundMusic() {
+        backgroundMusicSource.Pause();
+    }
+
+    public void StopBackgroundMusic() {
+        backgroundMusicSource.Stop();
+    }
+
+    public void SetBackgroundMusicVolume(float volume) {
+        backgroundMusicSource.volume = volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
